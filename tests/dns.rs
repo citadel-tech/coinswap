18e543eee16a2548a2e89e6f563860532a5fe6e7
keraliss <keralis1131f@gmail.com>
2024-08-21 13:53:53 +0530

    test_dns function updated

--- tests/dns.rs
+++ tests/dns.rs
@@ -1,9 +1,8 @@
-use coinswap::market::directory::{start_directory_server, DirectoryServer};
-use coinswap::utill::ConnectionType;
 use std::collections::HashSet;
 use std::fs;
 use std::path::PathBuf;
-use std::sync::{mpsc, Arc, RwLock};
+use std::process::Command;
+use std::sync::{Arc, RwLock};
 use std::thread;
 
 #[test]
@@ -24,32 +23,18 @@ fn test_dns() {
     .unwrap();
 
     // spawn the directoryd thread and get the log via mpsc channel.
-
-    let directory_server_instance = Arc::new(
-        DirectoryServer::new(Some(config_path.clone()), Some(ConnectionType::CLEARNET)).unwrap(),
-    );
-    let (tx, rx) = mpsc::channel();
-
-    let directory_server_instance_clone = directory_server_instance.clone();
-
-    thread::spawn(move || {
-        start_directory_server(directory_server_instance_clone);
-        tx.send("Server started".to_string()).unwrap();
-    });
+    let _directoryd_process = Command::new("cargo")
+        .args(["run", "--bin", "directoryd"])
+        .spawn()
+        .expect("Failed to start directoryd process");
 
     thread::sleep(std::time::Duration::from_secs(2));
 
-    match rx.recv() {
-        Ok(log_message) => println!("{}", log_message),
-        Err(e) => eprintln!("Failed to receive log message: {}", e),
-    }
-
-    // get the address of the server (local/tor).
     let addresses: Arc<RwLock<HashSet<String>>> = Arc::new(RwLock::new(HashSet::new()));
 
     let address = addresses.read().unwrap().iter().next().cloned().unwrap();
 
     // assert that address is of specific value.
-    let expected_address = "http://localhost:8080"; // Replace with the actual expected address
+    let expected_address = "http://localhost:8080";
     assert_eq!(address, expected_address);
 }
