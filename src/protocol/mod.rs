56aafe192429d2e9501c1c919b00b592d739a2ed
Harsh Mahajan <007harshmahajan@gmail.com>
2025-01-31 19:32:47 +0530

    just scaffolding

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,10 +1,9 @@
-//! Defines the Contract Transaction and Protocol Messages.
-
+#![allow(missing_docs)]
 pub(crate) mod contract;
 pub mod error;
 pub mod messages;
 pub mod taproot;
-
+pub mod musig2;
 pub(crate) use contract::Hash160;
 
 pub use messages::{DnsMetadata, DnsRequest};
