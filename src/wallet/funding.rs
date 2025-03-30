e6d944871710822b6592b5ade6d822d84a536a57
neoz666 <neoz.blockchain@gmail.com>
2025-03-30 10:03:25 +0530

    Logged fee and feerate
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>

--- src/wallet/funding.rs
+++ src/wallet/funding.rs
@@ -140,12 +140,18 @@ impl Wallet {
         let mut payment_output_positions = Vec::<u32>::new();
         let mut total_miner_fee = 0;
 
-        for (address, &output_value) in destinations
-            .iter()
-            .zip(output_values.iter())
-        {
+        for (address, &output_value) in destinations.iter().zip(output_values.iter()) {
             let remaining = Amount::from_sat(output_value);
             let selected_utxo = self.coin_select(remaining)?;
+
+            // Calculate total input amount
+            let total_input_amount =
+                selected_utxo
+                    .iter()
+                    .fold(Amount::ZERO, |acc, (unspent, _)| {
+                        acc.checked_add(unspent.amount)
+                            .expect("Amount sum overflowed")
+                    });
 
             // Prepare coins for spend_coins API
             let coins_to_spend = selected_utxo
@@ -154,18 +160,33 @@ impl Wallet {
                 .map(|(unspent, spend_info)| (unspent.clone(), spend_info.clone()))
                 .collect::<Vec<_>>();
 
-            // Create destination with output and change address
-            let destination = Destination::Multi(vec![
-                (address.clone(), Amount::from_sat(output_value)),
-            ]);
+            // Create destination with output
+            let destination =
+                Destination::Multi(vec![(address.clone(), Amount::from_sat(output_value))]);
 
             // Creates and Signs Transactions
             let funding_tx =
                 self.spend_coins(&coins_to_spend, destination, fee_rate.to_sat() as f64)?;
 
-            log::info!("Created Funding tx, txid : {}", funding_tx.compute_txid(),);
+            // Calculate actual fee
+            let actual_fee = total_input_amount
+                - (funding_tx.output.iter().fold(Amount::ZERO, |a, txo| {
+                    a.checked_add(txo.value)
+                        .expect("output amount summation overflowed")
+                }));
 
-            // Lock Mechanism.
+            let tx_size = funding_tx.weight().to_vbytes_ceil();
+            let actual_feerate = actual_fee.to_sat() as f32 / tx_size as f32;
+
+            log::info!(
+                "Created Funding tx, txid: {} | Size: {} vB | Fee: {} sats | Feerate: {:.2} sat/vB",
+                funding_tx.compute_txid(),
+                tx_size,
+                actual_fee.to_sat(),
+                actual_feerate
+            );
+
+            // TODO : Discussion on Lock Mechanism
             self.rpc.lock_unspent(
                 &funding_tx
                     .input
