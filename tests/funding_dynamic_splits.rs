26496c0ab9a60247c35850edce33089bb3b23928
neoz666 <neoz.blockchain@gmail.com>
2025-07-10 15:03:08 +0530

    Fixes for unwrap
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>

--- tests/funding_dynamic_splits.rs
+++ tests/funding_dynamic_splits.rs
@@ -104,52 +104,45 @@ fn test_create_funding_txn_with_varied_distributions(
         println!("\nTarget = {}", Amount::to_sat(target));
 
         // Call create_funding_txes with the generated addresses
-        match taker.get_wallet_mut().create_funding_txes_regular_swaps(
-            false,
-            target,
-            destinations.clone(),
-            fee_rate,
-        ) {
-            Ok(result) => {
-                for tx in result.funding_txes.iter() {
-                    let selected_inputs = tx
-                        .input
+        let result = taker
+            .get_wallet_mut()
+            .create_funding_txes_regular_swaps(false, target, destinations.clone(), fee_rate)
+            .unwrap();
+
+        for tx in result.funding_txes.iter() {
+            let selected_inputs = tx
+                .input
+                .iter()
+                .map(|txin| {
+                    taker
+                        .get_wallet()
+                        .list_all_utxo_spend_info()
+                        .unwrap()
                         .iter()
-                        .map(|txin| {
-                            taker
-                                .get_wallet()
-                                .list_all_utxo_spend_info()
-                                .unwrap()
-                                .iter()
-                                .find(|(utxo, _)| {
-                                    txin.previous_output.txid == utxo.txid
-                                        && txin.previous_output.vout == utxo.vout
-                                })
-                                .map(|(u, _)| u.amount)
-                                .expect("should find utxo")
+                        .find(|(utxo, _)| {
+                            txin.previous_output.txid == utxo.txid
+                                && txin.previous_output.vout == utxo.vout
                         })
-                        .collect::<Vec<_>>();
+                        .map(|(u, _)| u.amount)
+                        .expect("should find utxo")
+                })
+                .collect::<Vec<_>>();
 
-                    // Assert no duplicate inputs
-                    assert!(selected_inputs.iter().all(|&x| selected_inputs
-                        .iter()
-                        .filter(|&&y| y == x)
-                        .count()
-                        == 1),);
+            // Assert no duplicate inputs
+            assert!(selected_inputs.iter().all(|&x| selected_inputs
+                .iter()
+                .filter(|&&y| y == x)
+                .count()
+                == 1),);
 
-                    println!("Inputs : {selected_inputs:?}");
-                    println!("Sum of Inputs: {:?}", {
-                        selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>()
-                    });
+            println!("Inputs : {selected_inputs:?}");
+            println!("Sum of Inputs: {:?}", {
+                selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>()
+            });
 
-                    let outputs = tx.output.iter().map(|o| o.value).collect::<Vec<_>>();
+            let outputs = tx.output.iter().map(|o| o.value).collect::<Vec<_>>();
 
-                    println!("Outputs: {outputs:?}");
-                }
-            }
-            Err(e) => {
-                println!("Failed to create funding transactions: {e:?}");
-            }
+            println!("Outputs: {outputs:?}");
         }
     }
 }
@@ -164,157 +157,152 @@ fn test_assert_outputs_inputs(
 
     taker.get_wallet_mut().sync_no_fail();
 
-    match taker.get_wallet().get_balances() {
-        Ok(balance) => {
-            println!(
-                "Total Wallet Spendable Balance : {}",
-                balance.spendable.to_sat()
-            );
-            let expected_total = UTXO_SETS[0].iter().sum::<u64>();
-            assert_eq!(
-                balance.spendable.to_sat(),
-                expected_total,
-                "Total wallet balance does not match expected total from UTXO set."
-            );
-        }
-        Err(e) => println!("Failed to get wallet balance: {e:?}"),
-    }
+    let balance = taker.get_wallet().get_balances().unwrap();
+    println!(
+        "Total Wallet Spendable Balance : {}",
+        balance.spendable.to_sat()
+    );
+    let expected_total = UTXO_SETS[0].iter().sum::<u64>();
+    assert_eq!(
+        balance.spendable.to_sat(),
+        expected_total,
+        "Total wallet balance does not match expected total from UTXO set."
+    );
+
     println!("Expected Fee Rate : {MIN_FEE_RATE}");
     println!("Target = {target}");
 
-    match taker.get_wallet_mut().create_funding_txes_regular_swaps(
-        false,
-        Amount::from_sat(target),
-        destinations.clone(),
-        fee_rate,
-    ) {
-        Ok(result) => {
-            for tx in result.funding_txes.iter() {
-                let selected_inputs = tx
-                    .input
+    let result = taker
+        .get_wallet_mut()
+        .create_funding_txes_regular_swaps(
+            false,
+            Amount::from_sat(target),
+            destinations.clone(),
+            fee_rate,
+        )
+        .unwrap();
+
+    for tx in result.funding_txes.iter() {
+        let selected_inputs = tx
+            .input
+            .iter()
+            .map(|txin| {
+                taker
+                    .get_wallet()
+                    .list_all_utxo_spend_info()
+                    .unwrap()
                     .iter()
-                    .map(|txin| {
-                        taker
-                            .get_wallet()
-                            .list_all_utxo_spend_info()
-                            .unwrap()
-                            .iter()
-                            .find(|(utxo, _)| {
-                                txin.previous_output.txid == utxo.txid
-                                    && txin.previous_output.vout == utxo.vout
-                            })
-                            .map(|(u, _)| u.amount)
-                            .expect("should find utxo")
+                    .find(|(utxo, _)| {
+                        txin.previous_output.txid == utxo.txid
+                            && txin.previous_output.vout == utxo.vout
                     })
-                    .collect::<Vec<_>>();
+                    .map(|(u, _)| u.amount)
+                    .expect("should find utxo")
+            })
+            .collect::<Vec<_>>();
 
-                // Assert no duplicate inputs
-                assert!(selected_inputs.iter().all(|&x| selected_inputs
-                    .iter()
-                    .filter(|&&y| y == x)
-                    .count()
-                    == 1),);
+        // Assert no duplicate inputs
+        assert!(selected_inputs.iter().all(|&x| selected_inputs
+            .iter()
+            .filter(|&&y| y == x)
+            .count()
+            == 1),);
 
-                let outputs = tx.output.iter().map(|o| o.value).collect::<Vec<_>>();
+        let outputs = tx.output.iter().map(|o| o.value).collect::<Vec<_>>();
 
-                // Assertions for Input
-                println!("Inputs : {selected_inputs:?}");
-                assert_eq!(
-                    selected_inputs.len(),
-                    5,
-                    "Expected 5 input, got {}",
-                    selected_inputs.len()
-                );
-                assert!(
-                    selected_inputs.contains(&Amount::from_sat(38012)),
-                    "Didn't find expected input amount 38012 SATS in inputs"
-                );
-                assert!(
-                    selected_inputs.contains(&Amount::from_sat(298092)),
-                    "Didn't find expected input amount 298092 SATS in inputs"
-                );
-                assert!(
-                    selected_inputs.contains(&Amount::from_sat(301909)),
-                    "Didn't find expected input amount 301909 SATS in inputs"
-                );
-                assert!(
-                    selected_inputs.contains(&Amount::from_sat(9091)),
-                    "Didn't find expected input amount 9091 SATS in inputs"
-                );
-                assert!(
-                    selected_inputs.contains(&Amount::from_sat(712971)),
-                    "Didn't find expected input amount 712971 SATS in inputs"
-                );
+        // Assertions for Input
+        println!("Inputs : {selected_inputs:?}");
+        assert_eq!(
+            selected_inputs.len(),
+            5,
+            "Expected 5 input, got {}",
+            selected_inputs.len()
+        );
+        assert!(
+            selected_inputs.contains(&Amount::from_sat(38012)),
+            "Didn't find expected input amount 38012 SATS in inputs"
+        );
+        assert!(
+            selected_inputs.contains(&Amount::from_sat(298092)),
+            "Didn't find expected input amount 298092 SATS in inputs"
+        );
+        assert!(
+            selected_inputs.contains(&Amount::from_sat(301909)),
+            "Didn't find expected input amount 301909 SATS in inputs"
+        );
+        assert!(
+            selected_inputs.contains(&Amount::from_sat(9091)),
+            "Didn't find expected input amount 9091 SATS in inputs"
+        );
+        assert!(
+            selected_inputs.contains(&Amount::from_sat(712971)),
+            "Didn't find expected input amount 712971 SATS in inputs"
+        );
 
-                // Assertions for Outputs
-                println!("Outputs: {outputs:?}");
-                assert_eq!(
-                    outputs.len(),
-                    4,
-                    "Expected 4 outputs, got {}",
-                    outputs.len()
-                );
+        // Assertions for Outputs
+        println!("Outputs: {outputs:?}");
+        assert_eq!(
+            outputs.len(),
+            4,
+            "Expected 4 outputs, got {}",
+            outputs.len()
+        );
 
-                // Here, we expect the Change Chunks and Target Chunks to be similar to each other in the range Input / outputs.len()
-                let input_sum = selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>();
-                let lower_bound = ((input_sum / 4) as f64 * 0.825) as u64;
-                let upper_bound = ((input_sum / 4) as f64 * 1.175) as u64;
-                assert!(
-                    outputs
-                        .iter()
-                        .all(|x| (lower_bound..=upper_bound).contains(&x.to_sat())),
-                    "All output chunks should be in range of Input/Outputs.len() ± 15 + 2.5%"
-                );
+        // Here, we expect the Change Chunks and Target Chunks to be similar to each other in the range Input / outputs.len()
+        let input_sum = selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>();
+        let lower_bound = ((input_sum / 4) as f64 * 0.825) as u64;
+        let upper_bound = ((input_sum / 4) as f64 * 1.175) as u64;
+        assert!(
+            outputs
+                .iter()
+                .all(|x| (lower_bound..=upper_bound).contains(&x.to_sat())),
+            "All output chunks should be in range of Input/Outputs.len() ± 15 + 2.5%"
+        );
 
-                println!("Sum of all Inputs : {:?}", {
-                    selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>()
-                });
-                println!("Sum of all Outputs : {:?}", {
-                    outputs.iter().map(|o| o.to_sat()).sum::<u64>()
-                });
+        println!("Sum of all Inputs : {:?}", {
+            selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>()
+        });
+        println!("Sum of all Outputs : {:?}", {
+            outputs.iter().map(|o| o.to_sat()).sum::<u64>()
+        });
 
-                // Assert that any two of the 4 elements of outputs will equate to the target
-                let mut found_pair = false;
+        // Assert that any two of the 4 elements of outputs will equate to the target
+        let mut found_pair = false;
 
-                for (i, &value1) in outputs.iter().enumerate() {
-                    for &value2 in outputs.iter().skip(i + 1) {
-                        if value1.to_sat() + value2.to_sat() == target {
-                            found_pair = true;
-                            println!(
-                                "Found Target Chunks : {} + {} = {}",
-                                value1.to_sat(),
-                                value2.to_sat(),
-                                target
-                            );
-                            break;
-                        }
-                    }
-                    if found_pair {
-                        break;
-                    }
+        for (i, &value1) in outputs.iter().enumerate() {
+            for &value2 in outputs.iter().skip(i + 1) {
+                if value1.to_sat() + value2.to_sat() == target {
+                    found_pair = true;
+                    println!(
+                        "Found Target Chunks : {} + {} = {}",
+                        value1.to_sat(),
+                        value2.to_sat(),
+                        target
+                    );
+                    break;
                 }
-
-                assert!(
-                    found_pair,
-                    "No pair of outputs equates to the target value."
-                );
-
-                // Asserting Fee is more than 2% range.
-                let actual_fee = selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>()
-                    - outputs.iter().map(|o| o.to_sat()).sum::<u64>();
-                let tx_size = tx.weight().to_vbytes_ceil();
-                let actual_feerate = actual_fee as f64 / tx_size as f64;
-                println!("Actual fee rate: {actual_feerate}");
-                assert!(
-                    actual_feerate >= MIN_FEE_RATE * 0.98,
-                    "Fee rate ({}) is less than 98% of MIN_FEE_RATE ({})",
-                    actual_feerate,
-                    MIN_FEE_RATE
-                );
+            }
+            if found_pair {
+                break;
             }
         }
-        Err(e) => {
-            println!("Failed to create funding transactions: {e:?}");
-        }
+
+        assert!(
+            found_pair,
+            "No pair of outputs equates to the target value."
+        );
+
+        // Asserting Fee is more than 2% range.
+        let actual_fee = selected_inputs.iter().map(|a| a.to_sat()).sum::<u64>()
+            - outputs.iter().map(|o| o.to_sat()).sum::<u64>();
+        let tx_size = tx.weight().to_vbytes_ceil();
+        let actual_feerate = actual_fee as f64 / tx_size as f64;
+        println!("Actual fee rate: {actual_feerate}");
+        assert!(
+            actual_feerate >= MIN_FEE_RATE * 0.98,
+            "Fee rate ({}) is less than 98% of MIN_FEE_RATE ({})",
+            actual_feerate,
+            MIN_FEE_RATE
+        );
     }
 }
