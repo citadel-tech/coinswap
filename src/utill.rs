07a8df264c65c4fd2b6d5afeee9df747b6b72344
Supremesv715 <minkuverma55@gmail.com>
2025-09-25 22:13:28 +0530

    moved send_message_with_prefix to utill.rs

--- src/utill.rs
+++ src/utill.rs
@@ -31,13 +31,11 @@ use std::str::FromStr;
 static LOGGER: OnceLock<()> = OnceLock::new();
 
 use crate::{
-    error::NetError,
-    protocol::{
+    error::NetError, protocol::{
         contract::derive_maker_pubkey_and_nonce,
         error::ProtocolError,
         messages::{FidelityProof, MultisigPrivkey},
-    },
-    wallet::{fidelity_redeemscript, FidelityError, SwapCoin, UTXOSpendInfo, WalletError},
+    }, taker::error::TakerError, wallet::{fidelity_redeemscript, FidelityError, SwapCoin, UTXOSpendInfo, WalletError}
 };
 
 const INPUT_CHARSET: &str =
@@ -202,6 +200,26 @@ pub fn send_message(
 ) -> Result<(), NetError> {
     let mut writer = BufWriter::new(socket_writer);
     let msg_bytes = serde_cbor::ser::to_vec(message)?;
+    let msg_len = (msg_bytes.len() as u32).to_be_bytes();
+    let mut to_send = Vec::with_capacity(msg_bytes.len() + msg_len.len());
+    to_send.extend(msg_len);
+    to_send.extend(msg_bytes);
+    writer.write_all(&to_send)?;
+    writer.flush()?;
+    Ok(())
+}
+
+/// This method adds a prefix for
+/// maker to identify if its
+/// taker or not
+pub fn send_message_with_prefix(
+    socket_writer: &mut TcpStream,
+    message: &impl serde::Serialize,
+) -> Result<(), TakerError> {
+    let mut writer = BufWriter::new(socket_writer);
+    let mut msg_bytes = Vec::new();
+    msg_bytes.push(0x01);
+    msg_bytes.extend(serde_cbor::to_vec(message)?);
     let msg_len = (msg_bytes.len() as u32).to_be_bytes();
     let mut to_send = Vec::with_capacity(msg_bytes.len() + msg_len.len());
     to_send.extend(msg_len);
